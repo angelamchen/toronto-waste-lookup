4d00658a1d2875a38fa616976228afafa1c3f7f5
angelachen <angelachen085@gmail.com>
2019-01-20 22:16:30 -0500

    Updates

---
+++ precache-manifest.8a88c7c1c011c7825f1d673269d55653.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Toronto-Waste-Lookup/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6348a9821e88abee6c8c",
+    "url": "/Toronto-Waste-Lookup/static/js/runtime~main.6348a982.js"
+  },
+  {
+    "revision": "3884550571185c4c3e3e",
+    "url": "/Toronto-Waste-Lookup/static/js/main.38845505.chunk.js"
+  },
+  {
+    "revision": "b1355629af9cc6e71e6d",
+    "url": "/Toronto-Waste-Lookup/static/js/1.b1355629.chunk.js"
+  },
+  {
+    "revision": "3884550571185c4c3e3e",
+    "url": "/Toronto-Waste-Lookup/static/css/main.9f40c215.chunk.css"
+  },
+  {
+    "revision": "b1355629af9cc6e71e6d",
+    "url": "/Toronto-Waste-Lookup/static/css/1.64219158.chunk.css"
+  },
+  {
+    "revision": "749b433da2367aaa13018b95422dde8a",
+    "url": "/Toronto-Waste-Lookup/index.html"
+  }
+];
